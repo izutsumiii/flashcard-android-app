20004d610bfe58c4d85a92d33ea9462fc5a1c2f3
rosy <rosyshrestha028@gmail.com>
2023-12-04 15:15:24 -0500

    Add feature to load words from db for flashcard and load prev and next words

--- app/src/main/java/com/example/flashcard/ui/card/CardFlip.java
+++ app/src/main/java/com/example/flashcard/ui/card/CardFlip.java
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.flashcard.R;
+import com.example.flashcard.db.DBHandler;
+import com.example.flashcard.modal.WordModel;
+import com.example.flashcard.ui.wordlist.WordList;
+
+import java.util.ArrayList;
 
 public class CardFlip extends AppCompatActivity {
     private AnimatorSet mSetRightOut;
@@ -15,7 +24,8 @@ public class CardFlip extends AppCompatActivity {
     private boolean mIsBackVisible = false;
     private View mCardFrontLayout;
     private View mCardBackLayout;
-
+    private int currentWordIndex;
+    private ArrayList<WordModel> wordModelArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,23 @@ public class CardFlip extends AppCompatActivity {
         findViews();
         loadAnimations();
         changeCameraDistance();
+        loadWords();
+        showWord();
+
+        Button btnPrevious = findViewById(R.id.btnPrevious);
+        Button btnNext = findViewById(R.id.btnNext);
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPreviousWord();
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNextWord();
+            }
+        });
     }
 
     private void changeCameraDistance() {
@@ -56,6 +83,39 @@ public class CardFlip extends AppCompatActivity {
             mSetRightOut.start();
             mSetLeftIn.start();
             mIsBackVisible = false;
+        }
+    }
+
+    private void loadWords() {
+        DBHandler dbHandler = new DBHandler(this);
+        int folderId = getFolderIdFromSharedPreferences();
+        wordModelArrayList = dbHandler.getWordsByFolderId(folderId);
+        currentWordIndex = 0;
+    }
+
+    private void showWord() {
+        TextView cardTextFront = findViewById(R.id.card_text_front);
+        TextView cardTextBack = findViewById(R.id.card_text_back);
+        cardTextFront.setText(wordModelArrayList.get(currentWordIndex).getWord());
+        cardTextBack.setText(wordModelArrayList.get(currentWordIndex).getMeaning());
+    }
+
+    private int getFolderIdFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("FolderPreferences", MODE_PRIVATE);
+        return sharedPreferences.getInt("currentFolderId", -1);
+    }
+
+    private void showPreviousWord() {
+        if (currentWordIndex > 0) {
+            currentWordIndex--;
+            showWord();
+        }
+    }
+
+    private void showNextWord() {
+        if (currentWordIndex < wordModelArrayList.size() - 1) {
+            currentWordIndex++;
+            showWord();
         }
     }
 }
