59aca27f137f7f5bc03d2abf0838a47fa231292f
rosy <rosyshrestha028@gmail.com>
2023-12-05 16:46:30 -0500

    Unused imports cleanup

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -5,24 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.flashcard.ui.card.CardFlip;
 import com.example.flashcard.ui.folders.FolderListActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.flashcard.databinding.ActivityMainBinding;
-import com.example.flashcard.ui.wordlist.WordList;
-
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
         Button viewFlashcardBtn = findViewById(R.id.viewFlashcardBtn);
         viewFlashcardBtn.setOnClickListener(new View.OnClickListener() {
